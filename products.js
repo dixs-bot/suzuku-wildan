a5f994e0970cc488593dc7e93f4310e51af9f489
dikiwahyudin <74003372+dixs-bot@users.noreply.github.com>
2026-03-14 01:17:23 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,154 +1,395 @@
 const products = [
 
+/* ================= FRONX ================= */
+
 {
- id:"new-carry",
- name:"New Carry Pickup",
- category:"commercial",
- image:"White-NEW_CARRY_PICK-UP.png",
+id:"fronx",
+name:"Suzuki Fronx",
+category:"passenger",
+image:"SUZUKI_FRONK.png",
+badge:"new",
 
- variants:[
+variants:[
 
- {
-  id:"fd",
-  name:"Pick Up FD",
-
-  otr:175500000,
-  otr2026:177900000
- },
-
- {
-  id:"fd-ac-ps",
-  name:"Pick Up FD AC PS",
-
-  otr:183500000,
-  otr2026:185900000
- },
-
- {
-  id:"wd",
-  name:"Pick Up WD",
-
-  otr:176600000,
-  otr2026:179000000
- },
-
- {
-  id:"wd-ac-ps",
-  name:"Pick Up WD AC PS",
-
-  otr:184400000,
-  otr2026:186800000
- }
-
- ]
-
+{
+id:"gl-mt",
+name:"GL MT",
+otr:264300000,
+otr2026:266800000
 },
 
 {
- id:"apv-blindvan",
- name:"APV Blind Van",
- category:"commercial",
- image:"APV BLIND VAN.jpg",
-
- variants:[
-
- {
-  id:"blindvan",
-
-  name:"Blind Van",
-
-  otr:186700000,
-  otr2026:187000000
- }
-
- ]
-
+id:"gl-at",
+name:"GL AT",
+otr:275300000,
+otr2026:278800000
 },
 
 {
- id:"xl7",
- name:"XL7 Hybrid",
- category:"passenger",
- image:"Suzuki_XL7_ALPHA_KURO_IVORY-KURO.png",
-
- variants:[
-
- {
-  id:"zeta-mt",
-
-  name:"Zeta MT",
-
-  otr:269900000,
-  otr2026:271500000
- },
-
- {
-  id:"zeta-at",
-
-  name:"Zeta AT",
-
-  otr:281100000,
-  otr2026:282500000
- },
-
- {
-  id:"beta-mt-hybrid",
-
-  name:"Beta MT Hybrid",
-
-  otr:297600000,
-  otr2026:299000000
- },
-
- {
-  id:"beta-at-hybrid",
-
-  name:"Beta AT Hybrid",
-
-  otr:308700000,
-  otr2026:310000000
- },
-
- {
-  id:"alpha-at-hybrid",
-
-  name:"Alpha AT Hybrid",
-
-  otr:319800000,
-  otr2026:321500000
- }
-
- ]
-
+id:"gx-mt",
+name:"GX MT",
+otr:286700000,
+otr2026:289900000
 },
 
 {
- id:"spresso",
- name:"S-Presso",
- category:"passenger",
- image:"SUZUKI_S PRESSO.png",
+id:"gx-at",
+name:"GX AT",
+otr:304900000,
+otr2026:308000000
+},
 
- variants:[
+{
+id:"sgx-at",
+name:"SGX AT",
+otr:331000000,
+otr2026:334000000
+}
 
- {
-  id:"mt",
+]
 
-  name:"S-Presso MT",
+},
 
-  otr:176600000,
-  otr2026:180000000
- },
+/* ================= ERTIGA ================= */
 
- {
-  id:"at",
+{
+id:"ertiga",
+name:"All New Ertiga",
+category:"passenger",
+image:"SUZUKI_ERTIGA.png",
 
-  name:"S-Presso AT",
+variants:[
 
-  otr:186600000,
-  otr2026:190000000
- }
+{
+id:"mc-gl-mt",
+name:"MC GL MT",
+otr:265350000
+},
 
- ]
+{
+id:"mc-gl-at",
+name:"MC GL AT",
+otr:276400000
+},
+
+{
+id:"mc-gx-mt",
+name:"MC GX MT",
+otr:282000000
+},
+
+{
+id:"mc-gx-at",
+name:"MC GX AT",
+otr:293000000
+}
+
+]
+
+},
+
+/* ================= XL7 ================= */
+
+{
+id:"xl7",
+name:"XL7 Hybrid",
+category:"passenger",
+image:"Suzuki_XL7_ALPHA_KURO_IVORY-KURO.png",
+
+variants:[
+
+{
+id:"zeta-mt",
+name:"Zeta MT",
+otr:269900000,
+otr2026:271500000
+},
+
+{
+id:"zeta-at",
+name:"Zeta AT",
+otr:281100000,
+otr2026:282500000
+},
+
+{
+id:"beta-mt-hybrid",
+name:"Beta MT Hybrid",
+otr:297600000,
+otr2026:299000000
+},
+
+{
+id:"beta-at-hybrid",
+name:"Beta AT Hybrid",
+otr:308700000,
+otr2026:310000000
+},
+
+{
+id:"alpha-mt-hybrid",
+name:"Alpha MT Hybrid",
+otr:308700000,
+otr2026:310000000
+},
+
+{
+id:"alpha-at-hybrid",
+name:"Alpha AT Hybrid",
+otr:319800000,
+otr2026:321500000
+},
+
+{
+id:"alpha-at-hybrid-2tone",
+name:"Alpha AT Hybrid 2 Tone",
+otr:321900000,
+otr2026:323500000
+},
+
+{
+id:"alpha-kuro",
+name:"Alpha Hybrid Kuro",
+otr:323800000,
+otr2026:325500000
+},
+
+{
+id:"alpha-kuro-2tone",
+name:"Alpha Hybrid 2 Kuro",
+otr:325900000,
+otr2026:327500000
+}
+
+]
+
+},
+
+/* ================= S-PRESSO ================= */
+
+{
+id:"spresso",
+name:"S-Presso",
+category:"passenger",
+image:"SUZUKI_S PRESSO.png",
+
+variants:[
+
+{
+id:"mt",
+name:"MT",
+otr:176600000
+},
+
+{
+id:"at",
+name:"AT",
+otr:186600000
+}
+
+]
+
+},
+
+/* ================= BALENO ================= */
+
+{
+id:"baleno",
+name:"New Baleno",
+category:"passenger",
+image:"BALENO.jpg",
+
+variants:[
+
+{
+id:"baleno-at",
+name:"Baleno AT",
+otr:293300000
+}
+
+]
+
+},
+
+/* ================= GRAND VITARA ================= */
+
+{
+id:"grand-vitara",
+name:"Grand Vitara GX",
+category:"passenger",
+image:"grand vitara GX.png",
+
+variants:[
+
+{
+id:"gx-at",
+name:"GX AT",
+otr:421900000
+},
+
+{
+id:"gx-at-2tone",
+name:"GX AT 2 Tone",
+otr:424900000
+}
+
+]
+
+},
+
+/* ================= JIMNY 3 DOOR ================= */
+
+{
+id:"jimny-3door",
+name:"Jimny 3 Door",
+category:"passenger",
+
+variants:[
+
+{
+id:"3door-mt",
+name:"3 Door MT",
+otr:511550000
+},
+
+{
+id:"3door-at",
+name:"3 Door AT",
+otr:524000000
+},
+
+{
+id:"3door-mt-2tone",
+name:"3 Door MT 2 Tone",
+otr:514850000
+},
+
+{
+id:"3door-at-2tone",
+name:"3 Door AT 2 Tone",
+otr:527300000
+}
+
+]
+
+},
+
+/* ================= JIMNY 5 DOOR ================= */
+
+{
+id:"jimny-5door",
+name:"Jimny 5 Door",
+category:"passenger",
+
+variants:[
+
+{
+id:"5door-mt",
+name:"5 Door MT",
+otr:496100000
+},
+
+{
+id:"5door-at",
+name:"5 Door AT",
+otr:508700000
+},
+
+{
+id:"5door-mt-2tone",
+name:"5 Door MT 2 Tone",
+otr:499350000
+},
+
+{
+id:"5door-at-2tone",
+name:"5 Door AT 2 Tone",
+otr:511900000
+}
+
+]
+
+},
+
+/* ================= APV ================= */
+
+{
+id:"apv",
+name:"APV",
+category:"commercial",
+
+variants:[
+
+{
+id:"blindvan",
+name:"Blind Van",
+otr:186700000,
+otr2026:187000000
+},
+
+{
+id:"ge-ps",
+name:"GE PS MT",
+otr:228200000,
+otr2026:229000000
+},
+
+{
+id:"gl",
+name:"GL MT",
+otr:236400000,
+otr2026:237500000
+},
+
+{
+id:"gx",
+name:"GX MT",
+otr:250500000,
+otr2026:251500000
+}
+
+]
+
+},
+
+/* ================= CARRY ================= */
+
+{
+id:"carry",
+name:"New Carry Pickup",
+category:"commercial",
+
+variants:[
+
+{
+id:"fd",
+name:"FD",
+otr:175500000,
+otr2026:177900000
+},
+
+{
+id:"fd-ac-ps",
+name:"FD AC PS",
+otr:183500000,
+otr2026:185900000
+},
+
+{
+id:"wd",
+name:"WD",
+otr:176600000,
+otr2026:179000000
+},
+
+{
+id:"wd-ac-ps",
+name:"WD AC PS",
+otr:184400000,
+otr2026:186800000
+}
+
+]
 
 }
 
