f5b38f29aeebe7b88c1fccc0527387696949d058
dikiwahyudin <74003372+dixs-bot@users.noreply.github.com>
2026-03-14 01:03:45 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,130 +1,69 @@
-/* ================================================================
-   DATA PRODUK SUZUKI
-   SUPPORT NIK 2025 & 2026
-================================================================ */
-
 const products = [
 
-/* ================= FRONX ================= */
-
 {
- id:"fronx",
- name:"Suzuki Fronx",
- category:"passenger",
- image:"SUZUKI_FRONK.png",
- badge:"new",
+ id:"new-carry",
+ name:"New Carry Pickup",
+ category:"commercial",
+ image:"White-NEW_CARRY_PICK-UP.png",
 
  variants:[
 
  {
-  id:"gl-mt",
-  name:"GL MT",
+  id:"fd",
+  name:"Pick Up FD",
 
-  price:{
-   nik2025:264300000,
-   nik2026:266800000
-  }
+  otr:175500000,
+  otr2026:177900000
  },
 
  {
-  id:"gl-at",
-  name:"GL AT",
+  id:"fd-ac-ps",
+  name:"Pick Up FD AC PS",
 
-  price:{
-   nik2025:275300000,
-   nik2026:278800000
-  }
+  otr:183500000,
+  otr2026:185900000
  },
 
  {
-  id:"gx-mt",
-  name:"GX MT",
+  id:"wd",
+  name:"Pick Up WD",
 
-  price:{
-   nik2025:286700000,
-   nik2026:289900000
-  }
+  otr:176600000,
+  otr2026:179000000
  },
 
  {
-  id:"gx-at",
-  name:"GX AT",
+  id:"wd-ac-ps",
+  name:"Pick Up WD AC PS",
 
-  price:{
-   nik2025:304900000,
-   nik2026:308000000
-  }
- },
-
- {
-  id:"sgx-at",
-  name:"SGX AT",
-
-  price:{
-   nik2025:331000000,
-   nik2026:334000000
-  }
+  otr:184400000,
+  otr2026:186800000
  }
 
  ]
 
 },
 
-/* ================= ERTIGA ================= */
-
 {
- id:"ertiga",
- name:"All New Ertiga",
- category:"passenger",
- image:"SUZUKI_ERTIGA.png",
+ id:"apv-blindvan",
+ name:"APV Blind Van",
+ category:"commercial",
+ image:"APV BLIND VAN.jpg",
 
  variants:[
 
  {
-  id:"mc-gl-mt",
-  name:"MC GL MT",
+  id:"blindvan",
 
-  price:{
-   nik2025:265350000,
-   nik2026:270000000
-  }
- },
+  name:"Blind Van",
 
- {
-  id:"mc-gl-at",
-  name:"MC GL AT",
-
-  price:{
-   nik2025:276400000,
-   nik2026:281000000
-  }
- },
-
- {
-  id:"mc-gx-mt",
-  name:"MC GX MT",
-
-  price:{
-   nik2025:282000000,
-   nik2026:287000000
-  }
- },
-
- {
-  id:"mc-gx-at",
-  name:"MC GX AT",
-
-  price:{
-   nik2025:293000000,
-   nik2026:298000000
-  }
+  otr:186700000,
+  otr2026:187000000
  }
 
  ]
 
 },
-
-/* ================= XL7 ================= */
 
 {
  id:"xl7",
@@ -136,131 +75,52 @@ const products = [
 
  {
   id:"zeta-mt",
+
   name:"Zeta MT",
 
-  price:{
-   nik2025:269900000,
-   nik2026:271500000
-  }
+  otr:269900000,
+  otr2026:271500000
  },
 
  {
   id:"zeta-at",
+
   name:"Zeta AT",
 
-  price:{
-   nik2025:281100000,
-   nik2026:282500000
-  }
+  otr:281100000,
+  otr2026:282500000
  },
 
  {
   id:"beta-mt-hybrid",
+
   name:"Beta MT Hybrid",
 
-  price:{
-   nik2025:297600000,
-   nik2026:299000000
-  }
+  otr:297600000,
+  otr2026:299000000
  },
 
  {
   id:"beta-at-hybrid",
+
   name:"Beta AT Hybrid",
 
-  price:{
-   nik2025:308700000,
-   nik2026:310000000
-  }
- },
-
- {
-  id:"alpha-mt-hybrid",
-  name:"Alpha MT Hybrid",
-
-  price:{
-   nik2025:308700000,
-   nik2026:310000000
-  }
+  otr:308700000,
+  otr2026:310000000
  },
 
  {
   id:"alpha-at-hybrid",
+
   name:"Alpha AT Hybrid",
 
-  price:{
-   nik2025:319800000,
-   nik2026:321500000
-  }
+  otr:319800000,
+  otr2026:321500000
  }
 
  ]
 
 },
-
-/* ================= BALENO ================= */
-
-{
- id:"baleno",
- name:"New Baleno",
- category:"passenger",
- image:"BALENO.jpg",
-
- variants:[
-
- {
-  id:"baleno-at",
-  name:"Baleno AT",
-
-  price:{
-   nik2025:293300000,
-   nik2026:298000000
-  }
- }
-
- ]
-
-},
-
-/* ================= GRAND VITARA ================= */
-
-{
- id:"grand-vitara",
- name:"Grand Vitara GX",
- category:"passenger",
- image:"grand vitara GX.png",
-
- variants:[
-
- {
-  id:"gx-at",
-
-  name:"GX AT",
-
-  price:{
-   nik2025:421900000,
-   nik2026:427000000
-  }
-
- },
-
- {
-  id:"gx-at-2tone",
-
-  name:"GX AT 2 Tone",
-
-  price:{
-   nik2025:424900000,
-   nik2026:429000000
-  }
-
- }
-
- ]
-
-},
-
-/* ================= S PRESSO ================= */
 
 {
  id:"spresso",
@@ -273,141 +133,19 @@ const products = [
  {
   id:"mt",
 
-  name:"MT",
+  name:"S-Presso MT",
 
-  price:{
-   nik2025:176600000,
-   nik2026:180000000
-  }
-
+  otr:176600000,
+  otr2026:180000000
  },
 
  {
   id:"at",
 
-  name:"AT",
+  name:"S-Presso AT",
 
-  price:{
-   nik2025:186600000,
-   nik2026:190000000
-  }
-
- }
-
- ]
-
-},
-
-/* ================= CARRY ================= */
-
-{
- id:"new-carry",
- name:"New Carry Pickup",
- category:"commercial",
- image:"White-NEW_CARRY_PICK-UP.png",
-
- variants:[
-
- {
-  id:"fd",
-  name:"PU FD",
-
-  price:{
-   nik2025:175500000,
-   nik2026:177900000
-  }
- },
-
- {
-  id:"fd-ac-ps",
-  name:"PU FD AC PS",
-
-  price:{
-   nik2025:183500000,
-   nik2026:185900000
-  }
- },
-
- {
-  id:"wd",
-  name:"PU WD",
-
-  price:{
-   nik2025:176600000,
-   nik2026:179000000
-  }
- },
-
- {
-  id:"wd-ac-ps",
-  name:"PU WD AC PS",
-
-  price:{
-   nik2025:184400000,
-   nik2026:186800000
-  }
- }
-
- ]
-
-},
-
-/* ================= APV ================= */
-
-{
- id:"apv",
- name:"APV",
- category:"commercial",
- image:"RED APV_ARENA.png",
-
- variants:[
-
- {
-  id:"blindvan",
-
-  name:"APV Blind Van",
-
-  price:{
-   nik2025:186700000,
-   nik2026:187000000
-  }
-
- },
-
- {
-  id:"ge-mt",
-
-  name:"APV GE PS MT",
-
-  price:{
-   nik2025:228200000,
-   nik2026:229000000
-  }
-
- },
-
- {
-  id:"gl-mt",
-
-  name:"APV GL MT",
-
-  price:{
-   nik2025:236400000,
-   nik2026:237500000
-  }
-
- },
-
- {
-  id:"gx-mt",
-
-  name:"APV GX MT",
-
-  price:{
-   nik2025:250500000,
-   nik2026:251500000
-  }
-
+  otr:186600000,
+  otr2026:190000000
  }
 
  ]
